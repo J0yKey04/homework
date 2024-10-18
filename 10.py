a6a3a0d9720b4c02ac0fce4c133885e03613de6b
J0yKey04 <riazanov.dv@phystech.edu>
2024-10-18 18:41:51 +0300

    Old code cleanup

--- 10.py
+++
@@ -1,8 +0,0 @@
-
-A = [0] * int(input())
-
-for i in range(len(A)):
-    A[i]= int(input())
-
-print(A)
-
--- 3.py
+++
@@ -1,9 +0,0 @@
-a = list(map(int, input().split()))
-b = 1
-for i in range(len(a)):
-    b = b * a[i]
-
-b = b ** (1/len(a))
-
-print(b)
-
